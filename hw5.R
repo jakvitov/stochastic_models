3d8048dcd93eb7c2a9a11d3a4970102e9440796f
Jakub Vítovec <jakvitov@gmail.com>
2024-03-15 10:47:17 +0100

    Minor fixes

--- hw5.R
+++ hw5.R
@@ -5,11 +5,12 @@ library("expm")
 AverageLimitMatrix <- function(P, n) {
     res = P%^%0  
   
-    for (i in range1:n){
+    for (i in 1:n){
       res = res + P%^%i
     }
-    res = res%/%(n+1)
+    res = res/(n+1)
     return(res)
 }
 
-
+test_p = matrix(c(0.2, 0.8, 0.4, 0.6), nrow=2, ncol=2, byrow=TRUE)
+AverageLimitMatrix(test_p, 10)
