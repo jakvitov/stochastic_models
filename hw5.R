1df3e56c34e8fa99c617768adecf9468f8596d91
Jakub Vítovec <jakvitov@gmail.com>
2024-03-15 11:06:41 +0100

    Fixed for n=0

--- hw5.R
+++ hw5.R
@@ -3,14 +3,15 @@ library("expm")
 
 #Returns average limit matrix, which consists of sum of P^i -> n devided by n+1
 AverageLimitMatrix <- function(P, n) {
-    res = P%^%0  
+    res = c(nrow(P):ncol(P))*0
   
-    for (i in 1:n){
+    for (i in 0:n){
       res = res + P%^%i
     }
     res = res/(n+1)
     return(res)
 }
 
-test_p = matrix(c(0.2, 0.8, 0.4, 0.6), nrow=2, ncol=2, byrow=TRUE)
-AverageLimitMatrix(test_p, 10)
+
+#test_p = matrix(c(0.2, 0.8, 0.4, 0.6), nrow=2, ncol=2, byrow=TRUE)
+#AverageLimitMatrix(test_p, 0)
